c5eab97af5d8a2457f3ac7913cd7d1c3da54063a
daemon-surge-suzuya <daemonsurges@gmail.com>
2023-11-09 18:34:14 +0300

    removed random and useless comments

--- default.nix
+++ default.nix
@@ -1,8 +1,6 @@
 with import <nixpkgs> {};
 
 let
-  # This is the WORST way to do dependencies
-  # We just specify the derivation the same way as before
   simplePackage = pkgs.writeShellScriptBin "lol-test" ''
   nix-shell -p lolcat --run "echo $1 | lolcat"
   '';
@@ -10,7 +8,5 @@ in
 stdenv.mkDerivation rec {
   name = "test-environment";
 
-  # Then we add curl & jq to the list of buildInputs for the shell
-  # So curl and jq will be added to the PATH inside the shell
-  buildInputs = [ simplePackage pkgs.jq pkgs.curl ];
+  buildInputs = [ simplePackage ];
 }
